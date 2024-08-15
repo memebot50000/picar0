df49f3d4f50e4480e459b1a306e5cc5a5cbd108c
memebot50000 <164225944+memebot50000@users.noreply.github.com>
2024-08-15 16:59:33 -0400

    Update rc_car_control.py

--- rc_car_control.py
+++ rc_car_control.py
@@ -18,7 +18,7 @@ def move_forward(t, power):
 def move_backward(t, power):
     '''drives car backward for t seconds at specified power percentage'''
     left_motor.backward(power)
-    right_motor.backward(power)
+    right_motor.forward(power)
     time.sleep(t)
     stop()
 
